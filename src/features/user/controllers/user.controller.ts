0b651696744aa3f7931e3dd2eaaa2732215cf201
auresolll <usool.203@gmail.com>
2023-12-11 18:00:08 +0700

    @

--- src/features/user/controllers/user.controller.ts
+++ src/features/user/controllers/user.controller.ts
@@ -7,27 +7,28 @@ import {
   Patch,
   Query,
   UseGuards,
-} from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
-import { Model, Types } from "mongoose";
-import { AuthNotRequired } from "src/features/auth/decorators/auth-not-required.decorator";
-import { OrderService } from "src/features/basket/services/order.service";
-import { Product } from "src/features/product/schemas/product.schema";
-import { ENUM_ROLE_TYPE, getAllRoles } from "src/shared/constants/role";
-import { getFieldIds } from "src/shared/utils/get-ids";
-import { RolesGuard } from "src/shared/utils/roles.guard";
-import { Follow } from "../schemas/follow.schema";
-import { User } from "../schemas/user.schema";
-import { UserService } from "../services/user.service";
-import { ParseObjectIdPipe } from "./../../../shared/pipe/parse-object-id.pipe";
-import { Roles } from "./../../../shared/utils/roles.decorator";
-import { JwtAuthGuard } from "./../../auth/guard/jwt-auth.guard";
-import { RatingService } from "./../../product/services/rating.service";
-import { CurrentUser } from "src/features/auth/decorators/current-user.decorator";
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Model, Types } from 'mongoose';
+import { AuthNotRequired } from 'src/features/auth/decorators/auth-not-required.decorator';
+import { OrderService } from 'src/features/basket/services/order.service';
+import { Product } from 'src/features/product/schemas/product.schema';
+import { ENUM_ROLE_TYPE, getAllRoles } from 'src/shared/constants/role';
+import { getFieldIds } from 'src/shared/utils/get-ids';
+import { RolesGuard } from 'src/shared/utils/roles.guard';
+import { Follow } from '../schemas/follow.schema';
+import { User } from '../schemas/user.schema';
+import { UserService } from '../services/user.service';
+import { ParseObjectIdPipe } from './../../../shared/pipe/parse-object-id.pipe';
+import { Roles } from './../../../shared/utils/roles.decorator';
+import { JwtAuthGuard } from './../../auth/guard/jwt-auth.guard';
+import { RatingService } from './../../product/services/rating.service';
+import { CurrentUser } from 'src/features/auth/decorators/current-user.decorator';
+import { UpdateRoleDto } from '../dtos/update-role.dto';
 
-@Controller("user")
-@ApiBearerAuth("accessToken")
+@Controller('user')
+@ApiBearerAuth('accessToken')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class UserController {
   constructor(
@@ -37,34 +38,34 @@ export class UserController {
 
     @InjectModel(User.name) private UserModel: Model<User>,
     @InjectModel(Product.name) private ProductModel: Model<Product>,
-    @InjectModel(Follow.name) private FollowModel: Model<Follow>
+    @InjectModel(Follow.name) private FollowModel: Model<Follow>,
   ) {}
 
-  @ApiTags("Private User")
+  @ApiTags('Private User')
   @Roles(...getAllRoles())
-  @Get(":userId")
-  async getUser(@Param("userId", new ParseObjectIdPipe()) id: string) {
+  @Get(':userId')
+  async getUser(@Param('userId', new ParseObjectIdPipe()) id: string) {
     return this.userService.filterUser(
-      await this.userService.validateUserById(new Types.ObjectId(id))
+      await this.userService.validateUserById(new Types.ObjectId(id)),
     );
   }
 
-  @ApiTags("Public User")
-  @Get("")
+  @ApiTags('Public User')
+  @Get('')
   @AuthNotRequired()
   async getAllUsers() {
     return await this.userService.getUsers();
   }
 
-  @ApiTags("Public User")
+  @ApiTags('Public User')
   @AuthNotRequired()
-  @Get("seller/:userId")
-  async getUserSeller(@Param("userId", new ParseObjectIdPipe()) id: string) {
+  @Get('seller/:userId')
+  async getUserSeller(@Param('userId', new ParseObjectIdPipe()) id: string) {
     const user = await this.UserModel.findById(id);
 
     const requiredRole = ENUM_ROLE_TYPE.SELLER;
     if (String(user.role) !== requiredRole)
-      throw new BadGatewayException("Người dùng không phải người bán");
+      throw new BadGatewayException('Người dùng không phải người bán');
 
     const totalProducts = await this.ProductModel.find({ creator: user._id });
 
@@ -103,18 +104,29 @@ export class UserController {
     };
   }
 
+  @ApiTags('Private User')
+  @AuthNotRequired()
+  @Patch('update-role')
+  updateRole(@Query() query: UpdateRoleDto) {
+    return this.UserModel.findByIdAndUpdate(query.userID, {
+      role: new Types.ObjectId(query.role),
+    });
+  }
+
   @Roles(ENUM_ROLE_TYPE.ADMINISTRATION)
   @Delete()
-  delete(@Query("userId", new ParseObjectIdPipe()) id: string) {
+  delete(@Query('userId', new ParseObjectIdPipe()) id: string) {
     return this.UserModel.findByIdAndUpdate(id, { deletedAt: new Date() });
   }
 
+  @ApiTags('Private Setting')
   @Roles(ENUM_ROLE_TYPE.SELLER)
-  @Patch()
-  updateBank(@CurrentUser() user: User, @Query("bank") bank: string) {
+  @Patch('bank')
+  updateBank(@CurrentUser() user: User, @Query('bank') bank: string) {
     return this.UserModel.findByIdAndUpdate(user._id, { bank });
   }
 
+  @ApiTags('Private Setting')
   @Roles(ENUM_ROLE_TYPE.SELLER)
   @Delete()
   deleteBank(@CurrentUser() user: User) {
--- src/features/user/dtos/update-role.dto.ts
+++ src/features/user/dtos/update-role.dto.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsMongoId, IsNotEmpty, IsString, Matches } from "class-validator";
-import { ENUM_ROLE_TYPE } from "src/shared/constants/role";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { ENUM_ROLE_TYPE } from 'src/shared/constants/role';
 
 export class UpdateRoleDto {
   @ApiProperty()
@@ -8,10 +8,10 @@ export class UpdateRoleDto {
   @IsMongoId()
   userID: string;
 
-  @ApiProperty({ enum: [ENUM_ROLE_TYPE.CUSTOMER, ENUM_ROLE_TYPE.SELLER] })
+  @ApiProperty({ enum: ENUM_ROLE_TYPE })
   @IsNotEmpty()
   @IsString()
-  role: string;
+  role: ENUM_ROLE_TYPE;
 }
 
 export class ResponseUpdateRoleDto extends UpdateRoleDto {}
