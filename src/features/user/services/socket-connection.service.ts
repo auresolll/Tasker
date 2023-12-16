0c66307171923c3a152b26ed5a8ccab62500d945
auresolll <usool.203@gmail.com>
2023-12-16 13:34:55 +0700

    @

--- src/features/user/services/socket-connection.service.ts
+++ src/features/user/services/socket-connection.service.ts
@@ -10,85 +10,83 @@ import { User } from '../schemas/user.schema';
 
 @Injectable()
 export class SocketConnectionService {
-    constructor(
-        @InjectModel(SocketConnection.name)
-        private socketConnectionModel: Model<SocketConnection>,
-        @Inject(forwardRef(() => UserService)) private userService: UserService,
-    ) {}
+  constructor(
+    @InjectModel(SocketConnection.name)
+    private socketConnectionModel: Model<SocketConnection>,
+    @Inject(forwardRef(() => UserService)) private userService: UserService,
+  ) {}
 
-    async create(socket: Socket, user: User) {
-        console.log(user, socket.id, hostname());
-        
-        const connection = await this.socketConnectionModel.create({
-            user: user._id,
-            socketId: socket.id,
-            serverHostname: hostname(),
-            serverPort: environments.port,
-        });
+  async create(socket: Socket, user: User) {
+    const connection = await this.socketConnectionModel.create({
+      user: user._id,
+      socketId: socket.id,
+      serverHostname: hostname(),
+      serverPort: environments.port,
+    });
 
-        if (!user.online) {
-            user.online = true;
+    if (!user.online) {
+      user.online = true;
 
-            await user.save();
-        }
-
-        return connection.populate('user');
+      await user.save();
     }
 
-    getAll(user: User) {
-        return this.socketConnectionModel.find({ user: user._id });
+    return connection.populate('user');
+  }
+
+  getAll(user: User) {
+    return this.socketConnectionModel.find({ user: user._id });
+  }
+
+  getAllByUsersID(usersID: string[]) {
+    return this.socketConnectionModel.find({
+      user: { $in: usersID },
+    });
+  }
+
+  getBySocket(socket: Socket) {
+    return this.socketConnectionModel
+      .findOne({ socketId: socket.id })
+      .populate('user');
+  }
+
+  async deleteAllConnections() {
+    await this.socketConnectionModel.deleteMany({
+      serverHostname: hostname(),
+      serverPort: environments.port,
+    });
+
+    const users = await this.userService.getOnlineUsers();
+
+    for (const user of users) {
+      const connections = await this.getAll(user);
+
+      if (connections.length === 0) {
+        user.online = false;
+
+        await user.save();
+      }
+    }
+  }
+
+  async delete(socket: Socket) {
+    const connection = await this.getBySocket(socket);
+
+    if (!connection) {
+      return;
     }
 
-    getAllByUsersID(usersID: string[]) {
-        return this.socketConnectionModel.find({
-            user: { $in: usersID },
-        });
+    await this.socketConnectionModel.findByIdAndDelete(connection._id);
+
+    const user = connection.user;
+
+    const connections = await this.getAll(user);
+
+    if (connections.length === 0) {
+      user.online = false;
+
+      await user.save();
     }
 
-    getBySocket(socket: Socket) {
-        return this.socketConnectionModel
-            .findOne({ socketId: socket.id })
-            .populate('user');
-    }
-
-    async deleteAllConnections() {
-        await this.socketConnectionModel.deleteMany({
-            serverHostname: hostname(),
-            serverPort: environments.port,
-        });
-
-        const users = await this.userService.getOnlineUsers();
-
-        for (const user of users) {
-            const connections = await this.getAll(user);
-
-            if (connections.length === 0) {
-                user.online = false;
-
-                await user.save();
-            }
-        }
-    }
-
-    async delete(socket: Socket) {
-        const connection = await this.getBySocket(socket);
-
-        if (!connection) {
-            return;
-        }
-
-        await this.socketConnectionModel.findByIdAndDelete(connection._id);
-
-        const user = connection.user;
-
-        const connections = await this.getAll(user);
-
-        if (connections.length === 0) {
-            user.online = false;
-
-            await user.save();
-        }
-
-        return connection;
-    }
+    return connection;
+  }
 }
