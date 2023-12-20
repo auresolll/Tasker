d153c3b6626d3506f4fd85f23dc31278a4d812a1
auresolll <usool.203@gmail.com>
2023-12-20 15:04:49 +0700

    @

--- src/features/user/controllers/settings.controller.ts
+++ src/features/user/controllers/settings.controller.ts
@@ -140,13 +140,14 @@ export class SettingsController {
     @CurrentUser() user: User,
     @Body() body: UpdateProfileDto,
   ) {
-    const { avatar, bio, birthday, name, username, website } = body;
+    const { avatar, bio, birthday, name, username, website, phone } = body;
     user.avatar = avatar;
     user.bio = bio;
     user.birthday = birthday;
     user.name = name;
     user.username = username;
     user.website = website;
+    user.phone = phone;
     return {
       isProfile: (await user.save()) ? true : false,
     };
